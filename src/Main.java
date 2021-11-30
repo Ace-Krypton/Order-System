0aeff4544b1bc880a09464afdc448435b4a3baae
Ace-Krypton <ramizna.code.edu.az>
2021-11-30 16:38:47 +0100

    #1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
+
+    //This is menu method, it is clear by its name that, this method shows the menu of Coffee Bar
+    public static void menu() {
+        System.out.println("\t\t\t\t+----------------------------------------------------+");
+        System.out.println("\t\t\t\t                Coffee Bar + BakeShop              ");
+        System.out.println("\t\t\t\t            1. Espresso           $2.50");
+        System.out.println("\t\t\t\t            2. Americano          $2.75");
+        System.out.println("\t\t\t\t            3. Cappuccino         $3.25");
+        System.out.println("\t\t\t\t            4. Latte              $3.50");
+        System.out.println("\t\t\t\t+----------------------------------------------------+");
+    }
+
+    public static void order() {
+
+    }
+
+    //This is the main method, so everything happens here
     public static void main(String[] args) {
-        
+        //Calling the menu
+        menu();
     }
 }
